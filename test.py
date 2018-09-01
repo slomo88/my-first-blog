53f71f0d41d30bfd99c91c9b7d1c94b971356ada
Samantha Lo Monaco <sam.lomonaco@gmail.com>
2018-09-01 13:18:37 +1000

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+print('Hello, Django girls!')
+me = {'name': 'Sam', 'country': 'Australia', 'pet': 'Zelda', 'coffee': 'V60', 'job': 'research assistant'}
+print(me)
+
+Sam = 29
+Katee = 27
+Lana = 26
+
+pals = [Sam, Katee, Lana]
+print(pals)
+pals.sort()
+print(pals)
+print(pals[0])
+print(pals[1])
+print(pals[2])
+print(len(me))
+print(me['pet'])
+print(me['coffee'])
---
+++ test2.py
@@ -0,0 +1,7 @@
+def hi(name):
+	print('Hi ' + name + '!')
+
+pals = ['Sam', 'Lana', 'Katee', 'Erin']
+for name in pals:
+	hi(name)
+	print('Next pal')
